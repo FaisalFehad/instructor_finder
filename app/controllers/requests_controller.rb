a9711d04a15b43137c18de550a6b715a9d36e6d0
FaisalFehad <faisalfehad@gmail.com>
2016-08-31 10:40:56 +0100

    Delete index partials and change the view for request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,7 @@
 class RequestsController < ApplicationController
 
   def index
+    # Shows all listed instructors by the created date.
+    @instructors = Instructor.order('created_at DESC')
   end
 end
