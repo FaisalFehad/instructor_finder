d939c8c98e088197188d3a1e5bb5543a854cd9a0
FaisalFehad <faisalfehad@gmail.com>
2016-10-17 17:41:49 +0100

    Add validations

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,5 +1,21 @@
 class Instructor < ApplicationRecord
+  ## Association
   has_many :requests
+
+  ## To let geocoded use the saved addess to fetch and add latitudes and longitudes attributes
   geocoded_by :address
+
+  ## Google api
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+
+  ## validations
+   validates :name, presence: true, length: { in:2..25 }
+   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+   validates :car, length: { maximum:15 }
+   validates :address, presence: true, length: { in:6..70 }
+
+
+  # manual or auto or both needs to be present
+  validates :auto, presence: true, unless: -> (instructor){ instructor.manual.present? }
+  validates :manual, presence: true, unless: -> (instructor){ instructor.auto.present? }
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,13 @@
 class Request < ApplicationRecord
+
+  ## Association
   belongs_to :instructor
+
+  ## Validations
+  validates :name, presence: true, length: { in:3..20 }
+  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ } length: { in:3..55 }
+  validates :phone, presence: true,numericality: { only_integer: true }, length: { in:11..15 }
+  validates :message, length: { maximum:250 }
+  validates :postcode, presence: true, length: { in: 6..8 }
+  validates :instructor_id, presence: true, numericality: true
 end
