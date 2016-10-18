09796e080d51074276262b22c4fe404bcf800876
FaisalFehad <faisalfehad@gmail.com>
2016-10-18 13:06:35 +0100

    Remove postcode validations as doesnt exist attribute- fix

--- app/models/request.rb
+++ app/models/request.rb
@@ -5,9 +5,8 @@ class Request < ApplicationRecord
 
   ## Validations
   validates :name, presence: true, length: { in:3..20 }
-  validates :email, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }, length: { in:3..55 }
-  validates :phone, presence: true,numericality: { only_integer: true }, length: { in:11..15 }
+  validates :email, presence: true, format: { with:/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }, length: { in:3..55 }
+  validates :phone, presence: true, numericality: { only_integer: true }, length: { in:11..15 }
   validates :message, length: { maximum:250 }
-  validates :postcode, presence: true, length: { in: 6..8 }
   validates :instructor_id, presence: true, numericality: true
 end
